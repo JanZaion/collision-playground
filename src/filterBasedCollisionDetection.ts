80772e9d5d74b6f5d45c78d1932532478d092660
Jan Zaion <62064996+JanZaion@users.noreply.github.com>
2024-12-06 16:29:19 +0100

    feat: add prefiltering

--- src/filterBasedCollisionDetection.ts
+++ src/filterBasedCollisionDetection.ts
@@ -1,13 +1,27 @@
 export const filterBasedCollisionDetection = (grates: Grate[], pickedValues: Grate, field: GrateField) => {
-  const filteredGrates = grates.filter((grate) => grate[field] === pickedValues[field]);
-  const collisions: GrateField[] = [];
+  const filteredGrates = Object.entries(pickedValues)
+    .filter(([, value]) => Boolean(value))
+    .reduce((filtered, [currentField, value]) => {
+      const wouldFilter = filtered.filter((grate) => grate[currentField as keyof Grate] === value);
 
-  // Get all keys except the input field
-  const fieldsToCheck = (Object.keys(pickedValues) as GrateField[]).filter((key) => key !== field);
+      // Skip this filter if it would result in empty array
+      return wouldFilter.length > 0 ? wouldFilter : filtered;
+    }, grates);
+
+  // console.log(filteredGrates);
+
+  const collisions: GrateField[] = [];
 
   if (!pickedValues[field]) {
     return collisions;
   }
+
+  // if (!filteredGrates.length) {
+  //   return collisions;
+  // }
+
+  // Get all keys except the input field
+  const fieldsToCheck = (Object.keys(pickedValues) as GrateField[]).filter((key) => key !== field);
 
   for (const fieldToCheck of fieldsToCheck) {
     const filteredByField = filteredGrates.filter((grate) => grate[fieldToCheck] === pickedValues[fieldToCheck]);
@@ -19,3 +33,9 @@ export const filterBasedCollisionDetection = (grates: Grate[], pickedValues: Gra
 
   return collisions;
 };
+
+// const filteredGrates = grates.filter((grate) =>
+//   Object.entries(pickedValues)
+//     .filter(([, value]) => Boolean(value))
+//     .every(([field, value]) => grate[field as keyof Grate] === value)
+// );
